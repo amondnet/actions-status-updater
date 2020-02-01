9840dcdff5046e5cc06ccea17b83ff1649239e2b
Minsu Lee <amond@amond.net>
2020-02-01 12:10:21 +0900

    first commit

--- src/installer.ts
+++ src/installer.ts
@@ -1,92 +1,90 @@
-// Load tempDirectory before it gets wiped by tool-cache
-let tempDirectory = process.env['RUNNER_TEMPDIRECTORY'] || '';
-import * as core from '@actions/core';
-import * as tc from '@actions/tool-cache';
-import * as os from 'os';
-import * as path from 'path';
-import * as util from 'util';
+import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+import * as os from 'os'
+import * as path from 'path'
+import * as util from 'util'
 
-let osPlat: string = os.platform();
-let osArch: string = os.arch();
+const osPlat: string = os.platform()
+const osArch: string = os.arch()
+// Load tempDirectory before it gets wiped by tool-cache
+let tempDirectory = process.env['RUNNER_TEMPDIRECTORY'] || ''
 
 if (!tempDirectory) {
-    let baseLocation;
-    if (process.platform === 'win32') {
-        // On windows use the USERPROFILE env variable
-        baseLocation = process.env['USERPROFILE'] || 'C:\\';
+  let baseLocation
+  if (process.platform === 'win32') {
+    // On windows use the USERPROFILE env variable
+    baseLocation = process.env['USERPROFILE'] || 'C:\\'
+  } else {
+    if (process.platform === 'darwin') {
+      baseLocation = '/Users'
     } else {
-        if (process.platform === 'darwin') {
-            baseLocation = '/Users';
-        } else {
-            baseLocation = '/home';
-        }
+      baseLocation = '/home'
     }
-    tempDirectory = path.join(baseLocation, 'actions', 'temp');
+  }
+  tempDirectory = path.join(baseLocation, 'actions', 'temp')
 }
 
+export async function getBinary(version: string): Promise<void> {
+  // check cache
+  let toolPath: string
+  toolPath = tc.find('github-status-updater', version)
 
-export async function getBinary(version: string) {
-    // check cache
-    let toolPath: string;
-    toolPath = tc.find('github-status-updater', version);
+  if (!toolPath) {
+    // download, extract, cache
+    toolPath = await acquireRelease(version)
+    core.debug(`github-status-updater tool is cached under ${toolPath}`)
+  }
 
-    if (!toolPath) {
-        // download, extract, cache
-        toolPath = await acquireRelease(version);
-        core.debug('Go tool is cached under ' + toolPath);
-    }
-
-    toolPath = path.join(toolPath, 'bin');
-    //
-    // prepend the tools path. instructs the agent to prepend for future tasks
-    //
-    core.addPath(toolPath);
+  toolPath = path.join(toolPath, 'bin')
+  //
+  // prepend the tools path. instructs the agent to prepend for future tasks
+  //
+  core.addPath(toolPath)
 }
 
 async function acquireRelease(version: string): Promise<string> {
-    //
-    // Download - a tool installer intimately knows how to get the tool (and construct urls)
-    //
-    let fileName: string = getFileName();
-    let downloadUrl: string = getDownloadUrl(version, fileName);
+  //
+  // Download - a tool installer intimately knows how to get the tool (and construct urls)
+  //
+  const fileName: string = getFileName()
+  const downloadUrl: string = getDownloadUrl(version, fileName)
 
-    core.debug('Downloading Binary from: ' + downloadUrl);
+  core.debug(`Downloading Binary from: ${downloadUrl}`)
 
-    let downloadPath: string | null = null;
-    try {
-        downloadPath = await tc.downloadTool(downloadUrl);
-    } catch (error) {
-        core.debug(error);
+  let downloadPath: string | null = null
+  try {
+    downloadPath = await tc.downloadTool(downloadUrl)
+  } catch (error) {
+    core.debug(error)
 
-        throw `Failed to download version ${version}: ${error}`;
-    }
+    // eslint-disable-next-line no-throw-literal
+    throw `Failed to download version ${version}: ${error}`
+  }
 
-    //
-    // Install into the local tool cache - node extracts with a root folder that matches the fileName downloaded
-    //
-    const toolRoot = path.join(downloadPath, fileName);
-    return await tc.cacheDir(toolRoot, 'github-status-updater', version);
+  //
+  // Install into the local tool cache - node extracts with a root folder that matches the fileName downloaded
+  //
+  const toolRoot = path.join(downloadPath, fileName)
+  return await tc.cacheDir(toolRoot, 'github-status-updater', version)
 }
 
 function getFileName(): string {
-    const arches: {[arch: string]: string} = {
-        x64: 'amd64',
-        arm: 'armv6l',
-        arm64: 'arm64',
-        default: '386'
-    };
+  const arches: {[arch: string]: string} = {
+    x64: 'amd64',
+    arm: 'armv6l',
+    arm64: 'arm64',
+    default: '386'
+  }
 
-    const platform: string = osPlat == 'win32' ? 'windows' : osPlat;
-    const arch: string = arches[osArch] || arches['default'];
-    const filename: string = util.format(
-        'github-status-updater_%s_%s',
-        platform,
-        arch
-    );
-
-    return filename;
+  const platform: string = osPlat === 'win32' ? 'windows' : osPlat
+  const arch: string = arches[osArch] || arches['default']
+  return util.format('github-status-updater_%s_%s', platform, arch)
 }
 
 function getDownloadUrl(version: string, filename: string): string {
-    return util.format('https://github.com/cloudposse/github-status-updater/releases/download/%s/$s',version,  filename);
+  return util.format(
+    'https://github.com/cloudposse/github-status-updater/releases/download/%s/$s',
+    version,
+    filename
+  )
 }
